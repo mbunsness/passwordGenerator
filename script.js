295c22136e9bf3ffcffda829e28febbf7d735fec
mbunsness <mbunsness@gmail.com>
2021-08-01 20:33:14 -0600

    Initial Commit including html css and javascript files

---
+++ script.js
@@ -0,0 +1,14 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+  var charCount = prompt("How many characters does your password need?")
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword());
