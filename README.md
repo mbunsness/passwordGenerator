705685c7749fa6653e5e55dcbfe98b822da8bdfa
mbunsness <mbunsness@gmail.com>
2021-08-01 22:25:46 -0600

    Updated javascript to make the password generator functional and added a README

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # passwordGenerator
+
+## Description
+
 Generates a random password based off of user input.
+
+Motivation: To apply my knowledge of native functions and practice programming custom functions
+
+Why: To explore the usefullness of functions in javascript and to make generating passwords easier
+
+Problem: How to generate random passwords given the users needs for character count, uppercase, numbers and special characters.
+
+What I learned: How to use a for loop to choose a random character from a list
+
+## Installation
+
+None, hosted on github.
+
+## Usage
+
+Press the Generate Password button and follow the on screen prompts. After the user answers the questions the program will generate the password.
+
+## License
+
+Free Use
--- script.js
+++ script.js
@@ -1,5 +1,50 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+function generatePassword(){
+  var charCount = prompt("How many characters does your password need?");
+  console.log("You password will contain " + charCount + " characters.");
+  var charset = "abcdefghijklmnopqrstuvwxyz"; 
+  
+  //uppercase check
+  var charuc = confirm("Would you like your password to contain uppercase letters? (Press okay for yes and cancel for no)");
+  if (charuc = true) {
+    console.log("You password will contain uppercase characters.");
+    var charset = charset + "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  }
+  else {
+    console.log("You password will not contain uppercase characters.");
+  }
+  //numeric check
+  var charnum = confirm("Would you like your password to contain numbers? (Press okay for yes and cancel for no)");
+  if (charnum = true) {
+    console.log("You password will contain numbers.");
+    charset = charset + "1234567890";
+  }
+  else {
+    console.log("You password will not contain numbers.");
+  }
+  //special character check
+  var charlc = confirm("Would you like your password to contain special characters? (Press okay for yes and cancel for no)");
+  if (charlc = true) {
+    console.log("You password will contain special characters.");
+    charset = charset + "!@#$%^&*?";
+  }
+  else {
+    console.log("You password will not contain special characters.");
+  }
+
+  for (var i= 0; i < charCount; i++){
+    console.log(i);
+    var passwordgen = charset.charAt(Math.floor(Math.random()*charset.length));
+    console.log(password);
+    
+    password = password + passwordgen;
+    console.log(password);
+  }
+  console.log("Character set is "+ charset);
+  console.log("Password is "+ password);
+  return password;
+}
 
 // Write password to the #password input
 function writePassword() {
@@ -7,8 +52,8 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-  var charCount = prompt("How many characters does your password need?")
+  
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword());
+generateBtn.addEventListener("click", writePassword);
